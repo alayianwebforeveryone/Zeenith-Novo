aef0b189e288405bb72505818e58d89fe194cc25
alayianwebforeveryone <alayiansaeed@gmail.com>
2024-09-25 23:08:39 +0500

    add form config

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+          background: '#182527',
+          headingColor: '#20CD8D',
+          textColor: '#D9D9D9',
+          active: '#20CD8D'
+          
+        },
+
+    },
+  },
+  plugins: [],
+}
+
